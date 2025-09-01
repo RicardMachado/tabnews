74258d146291b81a4c82e5a19e58118f02d679a5
Ricardo Machado <ricardo.machado10.rm@gmail.com>
2025-09-01 10:17:11 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/controller.js
+++ infra/controller.js
@@ -1,18 +1,26 @@
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import {
+  InternalServerError,
+  MethodNotAllowedError,
+  ValidationError,
+} from "infra/errors.js";
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
 
 function onErrorHandler(error, request, response) {
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error);
+  }
+
   const puplicErrorObject = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
   });
 
   console.error(puplicErrorObject);
-  response.status(500).json(puplicErrorObject);
-}
-
-function onNoMatchHandler(request, response) {
-  const publicErrorObject = new MethodNotAllowedError();
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+  response.status(puplicErrorObject.statusCode).json(puplicErrorObject);
 }
 
 const controller = {
